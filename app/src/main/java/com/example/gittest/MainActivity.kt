3552d53c80bd29e1bffb56cb0ebc190368689adb
n.smyk <n.smyk@smartweb.com.ua>
2022-01-18 13:12:36 +0200

    Create v1

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name! RRR")
+    Text(text = "Hello $name! ttt")
 }
 
 @Preview(showBackground = true)
